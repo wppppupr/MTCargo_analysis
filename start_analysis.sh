a719b6e2ca6f009f46aa05609c6b8bbcbaced559
sasaki_mac <0704waffle@gmail.com>
2026-05-13 17:48:17 +0900

    a

--- start_analysis.sh
+++ start_analysis.sh
@@ -1,29 +1,12 @@
-TARGET_DIR='/Volumes/My Passport/Sasaki/MTsingleBeads/20260122/exp001'
-TARGET_DIR1='/Volumes/My Passport/Sasaki/MTsingleBeads/20260122/exp'
+TARGET_DIR='/Volumes/My Passport/Sasaki/MTsingleBeads/20260122/exp'
+TARGET_DIR1='/Volumes/My Passport/Sasaki/MTsingleBeads/20260121/beads_trans_crop_crop'
 TARGET_DIR2='/Volumes/My Passport/Sasaki/MTsingleBeads/20260121/exp_crop1'
 
+pixi run python libs/calc_local_polar.py "${TARGET_DIR}"
+pixi run python libs/calc_bg_polar.py "${TARGET_DIR}"
 
-pixi run python libs/optical_flow.py \
-    "${TARGET_DIR}/GFP"\
-    "${TARGET_DIR}/Farneback.h5"
+pixi run python libs/calc_local_polar.py "${TARGET_DIR1}"
+pixi run python libs/calc_bg_polar.py "${TARGET_DIR1}"
 
-pixi run python libs/evaluate_of.py \
-    --h5_path "${TARGET_DIR}/Farneback.h5" \
-    --csv_path "${TARGET_DIR}/MTtrack.csv"
-
-
-pixi run python libs/optical_flow.py \
-    "${TARGET_DIR1}/GFP"\
-    "${TARGET_DIR1}/Farneback.h5"
-
-pixi run python libs/evaluate_of.py \
-    --h5_path "${TARGET_DIR1}/Farneback.h5" \
-    --csv_path "${TARGET_DIR1}/MTtrack.csv"
-
-pixi run python libs/optical_flow.py \
-    "${TARGET_DIR2}/GFP"\
-    "${TARGET_DIR2}/Farneback.h5"
-
-pixi run python libs/evaluate_of.py \
-    --h5_path "${TARGET_DIR2}/Farneback.h5" \
-    --csv_path "${TARGET_DIR2}/MTtrack.csv"
+pixi run python libs/calc_local_polar.py "${TARGET_DIR2}"
+pixi run python libs/calc_bg_polar.py "${TARGET_DIR2}"
