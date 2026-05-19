359f30a28301af79448a3acbf784a4a0bb043d58
sasaki_kawamataPC02 <0704waffle@gmail.com>
2026-05-19 14:47:56 +0900

    a

--- start_analysis.sh
+++ start_analysis.sh
@@ -3,18 +3,20 @@ TARGET_DIR0='/mnt/SSD/Sasaki/MTsingleBeads/20260122/exp001'
 TARGET_DIR1='/mnt/SSD/Sasaki/MTsingleBeads/20260121/beads_trans_crop_crop'
 TARGET_DIR2='/mnt/SSD/Sasaki/MTsingleBeads/20260121/exp_crop1'
 
+<<comment
 #pixi run python libs/optical_flow.py "${TARGET_DIR}/GFP" "${TARGET_DIR}/Farneback.h5"
 pixi run python libs/optical_flow.py "${TARGET_DIR0}/GFP" "${TARGET_DIR0}/Farneback.h5"
 pixi run python libs/optical_flow.py "${TARGET_DIR1}/GFP" "${TARGET_DIR1}/Farneback.h5"
 pixi run python libs/optical_flow.py "${TARGET_DIR2}/GFP" "${TARGET_DIR2}/Farneback.h5"
-
-<<comment
-pixi run python libs/calc_local_polar.py "${TARGET_DIR}"
-pixi run python libs/calc_bg_polar.py "${TARGET_DIR}"
-
-pixi run python libs/calc_local_polar.py "${TARGET_DIR1}"
-pixi run python libs/calc_bg_polar.py "${TARGET_DIR1}"
-
-pixi run python libs/calc_local_polar.py "${TARGET_DIR2}"
-pixi run python libs/calc_bg_polar.py "${TARGET_DIR2}"
 comment
+
+pixi run python libs/calc_local_polar.py "${TARGET_DIR}" --particle_radius 6
+
+pixi run python libs/calc_local_polar.py "${TARGET_DIR0}" --particle_radius 6
+#pixi run python libs/calc_bg_polar.py "${TARGET_DIR0}"
+
+pixi run python libs/calc_local_polar.py "${TARGET_DIR1}" --particle_radius 6
+#pixi run python libs/calc_bg_polar.py "${TARGET_DIR1}"
+
+pixi run python libs/calc_local_polar.py "${TARGET_DIR2}" --particle_radius 6
+#pixi run python libs/calc_bg_polar.py "${TARGET_DIR2}"
