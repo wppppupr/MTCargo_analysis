c80d2e7d2f316a64945ba9b472aa84eb83b48d06
sasaki_mac <0704waffle@gmail.com>
2026-06-09 14:33:13 +0900

    a

--- start_analysis.sh
+++ start_analysis.sh
@@ -1,14 +1,14 @@
-TARGET_DIR='/Volumes/data/Sasaki/MTsingleBeads/beads3um/20260603'
+TARGET_DIR='/Volumes/data/Sasaki/MTsingleBeads/beads7um/20260608'
 H5_FILE='TRITC_flows.h5'
 
 for FILE in "${TARGET_DIR}"/*; do
     [ -d "$FILE" ] || continue
-    
+
     BASENAME=$(basename "$FILE")
 
     echo "Processing $BASENAME..."
 
-    pixi run python libs/calc_local_polar.py "${TARGET_DIR}/${BASENAME%.nd2}" --particle_radius 6 --h5_file "${H5_FILE}"
+    pixi run python libs/calc_local_polar.py "${TARGET_DIR}/${BASENAME%.nd2}" --particle_radius 36 --h5_file "${H5_FILE}"
     pixi run python libs/calc_bg_polar.py "${TARGET_DIR}/${BASENAME%.nd2}" --h5_file "${H5_FILE}"
 
 done
